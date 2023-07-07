0e17ea5536ca706136cc010d8952737bf556bb10
Francisco Javier Galán Guerrero <galan.guererro@bootcamp-upgrade.com>
2023-07-07 13:27:30 +0200

    exercises done

--- projects/exercise2/src/app/app-routing.module.ts
+++ projects/exercise2/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { AppComponent } from './app.component';
 import { SecureGuard } from './secure/secure.guard';
 
 const routes: Routes = [
+  // creacion de las rutas con el lazy loadind
   {
     path: 'lazy',
+    //en cada ruta importa el modulo correspondiente
     loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule),
   },
   {
     path: 'secure',
     loadChildren: () => import('./secure/secure.module').then((m) => m.SecureModule),
+    //añadido el guard para que sea imposible acceder a la ruta
     canActivate: [SecureGuard],
   },
 ];
